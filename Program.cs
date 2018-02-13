1f67df0424289b18dc3e7c4c712277bc25a8526d
Kristiāns Cīmurs <k.cimurs@yahoo.com>
2018-02-13 20:59:39 +0200

    first commot

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //izsaucam pirmo izveidoto funkciju
+            SayHello();
+            //otrā funckcija
+            SayHellotoUser();
+            VardsunVecums();
+        }
+
+
+
+
+            
+        //pirmā funckija
+        static void SayHello()
+        {
+            Console.WriteLine("Hello World !! @@@##");
+            Console.ReadLine();
+        }
+        //otrā funckcija - izveidots mainīgais 
+        static void SayHellotoUser()
+        {
+            Console.WriteLine("Ieraksti savu vārdu");
+            string userName;
+            userName = Console.ReadLine();
+            Console.WriteLine("Labdien "+userName+"!");
+            Console.ReadLine();
+        }
+
+        static void VardsunVecums()
+        {
+            string vards;
+            string vecums;
+            Console.WriteLine("Vārds?");
+            vards = Console.ReadLine();
+            Console.WriteLine("Vecums?");
+            vecums = Console.ReadLine();
+            Console.WriteLine("Vārds "+vards+" Vecums "+vecums);
+            Console.ReadLine();
+        }
+    }
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # RCSchool
+Mācos kodēt 
